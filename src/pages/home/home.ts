f487a4068363ef860caa1fe66b5bd49dceaf2672
pasthortown <pastorchessmaster@gmail.com>
2018-06-22 11:34:19 -0500

    de binario a decimal

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,7 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  numero1: number;
-  numero2: number;
+  numero1: string;
   resultado: number;
 
   constructor(public navCtrl: NavController) {
@@ -16,6 +15,30 @@ export class HomePage {
   }
 
   calcular() {
-    this.resultado = this.numero1*1 + this.numero2*1;
+    let aSumar = [];
+    let largo = this.numero1.length;
+    let elemento = 1;
+    let i = 1;
+    aSumar.push(elemento);
+    while(i<largo){
+      elemento = elemento*2;
+      aSumar.push(elemento);
+      i = i + 1;
+    }
+    let aSumarCorrecto = [];
+    i = 1;
+    while(i<=largo){
+      elemento = aSumar[largo-i];
+      aSumarCorrecto.push(elemento);
+      i = i + 1;
+    }
+    i = 0;
+    this.resultado = 0;
+    while(i<largo){
+      if ( this.numero1[i] == '1' ){
+        this.resultado = this.resultado + aSumarCorrecto[i];
+      }
+      i = i + 1;
+    }
   }
 }
